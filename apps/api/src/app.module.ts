992de8de0234265c758284f5eda54a9c3e579ca4
bouajilaprog <bouajilamedyessine@gmail.com>
2026-03-24 12:20:39 +0100

    feat(backend:init): add root AppModule

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { DbModule } from './db/db.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.local', '.env'],
+    }),
+    DbModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
