7c39415612cddccbe702ebc7ca196f0f9b837033
bouajilaprog <bouajilamedyessine@gmail.com>
2026-03-24 12:20:54 +0100

    feat(backend:db): add schema exports

---
+++ apps/api/src/db/schema/index.ts
@@ -0,0 +1,16 @@
+// Tables
+export { users, usersRelations, type User, type NewUser } from './users';
+export {
+  organizations,
+  organizationsRelations,
+  type Organization,
+  type NewOrganization,
+} from './organizations';
+export {
+  memberships,
+  membershipsRelations,
+  membershipRoleEnum,
+  type Membership,
+  type NewMembership,
+  type MembershipRole,
+} from './memberships';
