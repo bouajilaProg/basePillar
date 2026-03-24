9602b81252c85af41789e4ab33dc43805bdd35e1
bouajilaprog <bouajilamedyessine@gmail.com>
2026-03-24 12:20:14 +0100

    feat(shared:ui): add package exports

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,7 @@
+// Utilities
+export { cn } from './lib/utils';
+
+// Components
+export { Button, buttonVariants, type ButtonProps } from './components/ui/button';
+export { Input, type InputProps } from './components/ui/input';
+export { Label, type LabelProps } from './components/ui/label';
