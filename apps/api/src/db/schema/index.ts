7bb9fecf64941bef9a1648db1d1fb9a8a0d23fad
bouajilaprog <bouajilamedyessine@gmail.com>
2026-04-01 17:38:56 +0100

    feat(api:db): export all new schema tables from index
    Exports filebases, filebaseMembers, folders, files, filePointers,
    userGroups, userGroupMembers, accessRules with all enums and types.
    Org/membership exports remain commented out for future use.

--- apps/api/src/db/schema/index.ts
+++ apps/api/src/db/schema/index.ts
@@ -17,3 +17,57 @@ export { users, usersRelations, type User, type NewUser } from './users';
 //   type NewMembership,
 //   type MembershipRole,
 // } from './memberships';
+
+// Filebases - user's personal file storage space
+export { filebases, filebasesRelations, type Filebase, type NewFilebase } from './filebases';
+
+// Filebase Members - users invited to a filebase
+export {
+  filebaseMembers,
+  filebaseMembersRelations,
+  filebaseMemberRoleEnum,
+  type FilebaseMember,
+  type NewFilebaseMember,
+  type FilebaseMemberRole,
+} from './filebaseMembers';
+
+// Folders - directory structure within a filebase
+export { folders, foldersRelations, type Folder, type NewFolder } from './folders';
+
+// Files - physical files stored in S3
+export { files, filesRelations, type File, type NewFile } from './files';
+
+// File Pointers - user-facing file entries (including shortcuts)
+export {
+  filePointers,
+  filePointersRelations,
+  type FilePointer,
+  type NewFilePointer,
+} from './filePointers';
+
+// User Groups - groups for bulk access control
+export { userGroups, userGroupsRelations, type UserGroup, type NewUserGroup } from './userGroups';
+
+// User Group Members - users in groups
+export {
+  userGroupMembers,
+  userGroupMembersRelations,
+  type UserGroupMember,
+  type NewUserGroupMember,
+} from './userGroupMembers';
+
+// Access Rules - fine-grained access control
+export {
+  accessRules,
+  accessRulesRelations,
+  targetTypeEnum,
+  subjectTypeEnum,
+  accessTypeEnum,
+  permissionEnum,
+  type AccessRule,
+  type NewAccessRule,
+  type TargetType,
+  type SubjectType,
+  type AccessType,
+  type Permission,
+} from './accessRules';
