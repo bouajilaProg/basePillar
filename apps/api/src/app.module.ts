7694104fbedcb6e3ca529a7aa24cb479bce27b10
bouajilaprog <bouajilamedyessine@gmail.com>
2026-04-26 22:02:21 +0100

    feat(app): register StarsModule in app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { FilesModule } from './files/files.module';
 import { DbModule } from './db/db.module';
+import { StarsModule } from './stars/stars.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { DbModule } from './db/db.module';
     DbModule,
     AuthModule,
     FilesModule,
+    StarsModule,
   ],
   controllers: [],
   providers: [],
