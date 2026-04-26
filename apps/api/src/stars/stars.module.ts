b09ca77e4bebfa734ccc47d4e2eb64b9b6423fdf
bouajilaprog <bouajilamedyessine@gmail.com>
2026-04-26 22:02:19 +0100

    feat(api:stars): configure stars module with controller and service

--- apps/api/src/stars/stars.module.ts
+++ apps/api/src/stars/stars.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { StarsService } from './stars.service';
+import { StarsController } from './stars.controller';
+import { FilebaseAccessGuard } from '../files/guards/filebaseAccess.guard';
 
 @Module({
-  providers: [StarsService]
+  imports: [AuthModule],
+  controllers: [StarsController],
+  providers: [StarsService, FilebaseAccessGuard],
+  exports: [StarsService],
 })
 export class StarsModule {}
